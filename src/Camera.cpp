fa5a7b47466825190cd7110a0ed177624f3157dd
J. Francisco <joaofhmunhoz@gmail.com>
2025-11-28 11:27:11 -0300

    camera lookat

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -4,38 +4,74 @@ Camera::Camera()
     : perspectiveProjection(true), fov(45.0f), nearplane(0.1f), farplane(100.0f),
       cameraDistance(1.0f), top(1.0f), bottom(-1.0f), right(1.0f), left(-1.0f)
 {
-    updateViewVector();
 }
 
-void Camera::updateViewVector()
-{
-    viewX = viewRadius * sin(viewTheta) * cos(viewPhi);
-    viewY = viewRadius * sin(viewPhi);
-    viewZ = viewRadius * cos(viewTheta) * cos(viewPhi);
-}
 
-void Camera::setPosition(float x, float y, float z)
+void Camera::setCamera(float x, float y, float z, float theta, float phi, bool lookat)
 {
     cameraX = x;
     cameraY = y;
     cameraZ = z;
+    cameraTheta = theta;
+    cameraPhi = phi;
+    cameraLookAt = lookat;
 }
 
-void Camera::setViewDirection(float theta, float phi)
+
+glm::vec4 Camera::getPosition() const
 {
-    viewTheta = theta;
-    viewPhi = phi;
-    updateViewVector();
+    float x, y, z;
+    if(cameraLookAt)
+    {
+        x = cameraX+cameraRadius*cos(cameraPhi)*sin(cameraTheta);
+        y = cameraY+cameraRadius*sin(cameraPhi);
+        z = cameraZ+cameraRadius*cos(cameraPhi)*cos(cameraTheta);
+    }
+    else
+    {
+        x = cameraX;
+        y = cameraY;
+        z = cameraZ;
+    }
+    return glm::vec4(x, y, z, 1.0f);
 }
 
-glm::vec3 Camera::getPosition() const
+glm::vec4 Camera::getView() const
 {
-    return glm::vec3(cameraX, cameraY, cameraZ);
+    float x, y, z;
+    if(cameraLookAt)
+    {
+        x =-cameraRadius*cos(cameraPhi)*sin(cameraTheta);
+        y =-cameraRadius*sin(cameraPhi);
+        z =-cameraRadius*cos(cameraPhi)*cos(cameraTheta);
+    }
+    else
+    {
+        x = cameraRadius*cos(cameraPhi)*sin(cameraTheta);
+        y = cameraRadius*sin(cameraPhi);
+        z = cameraRadius*cos(cameraPhi)*cos(cameraTheta);
+    }
+    return glm::vec4(x, y, z, 0.0f);
 }
 
-glm::vec3 Camera::getViewDirection() const
+float Camera::getTheta()
 {
-    return glm::vec3(viewX, viewY, viewZ);
+    return cameraTheta;
+}
+
+float Camera::getPhi()
+{
+    return cameraPhi;
+}
+
+bool Camera::isLookAt()
+{
+    return cameraLookAt;
+}
+
+glm::vec4 Camera::getUpVector() const
+{
+    return glm::vec4(0.0f, 1.0f, 0.0f, 0.0f);
 }
 
 void Camera::setPerspective(bool perspective)
--- src/Player.hpp
+++ src/Player.hpp
@@ -4,18 +4,42 @@
 #include "Camera.hpp"
 #include <glm/glm.hpp>
 
+#define PLAYER_VELOCITY 5.0f
+#define SLOWED_VELOCITY 2.0f
+#define CAMERA_HEIGHT 2.0f
+
+typedef struct input
+{
+    bool front=false;
+    bool back=false;
+    bool right=false;
+    bool left=false;
+    float theta=0.0f;
+    float phi=0.0f;
+
+    bool aiming=false;
+    bool lookat=false;
+} input_t;
+
 class Player
 {
 public:
     Player();
-    void setPosition(float x, float y, float z);
+
+    void update(float deltaTime, input_t input);
+
     glm::vec3 getPosition() const;
-    void update(float deltaTime);
-    int getMaxAmmo() const;
-    void shoot(float time);
-    void reload();
     int getAmmo() const;
     int getScore() const;
+    int getMaxAmmo() const;
+    glm::vec3 getNextPosition(float deltaTime, input_t input);
+
+    void setPosition(float x, float y, float z);
+    void setViewDirection(float theta, float phi);
+    void setColision(bool collided);
+
+    void shoot(float time);
+    void reload();
     void addScore(int points);
     float gunAnimation(float time);
 
@@ -26,11 +50,17 @@ private:
     float posX = 0.0f;
     float posY = 0.0f;
     float posZ = 0.0f;
+    float velocity;
+    bool collision = false;
+
+    float viewRadius = 2.5f;
+    float viewTheta;
+    float viewPhi;
 
     Camera camera;
-    int nroBalas = 6;
-    int pontuacao = 0;
-    int maxAmmo = 6;
+    int nroBalas;
+    int pontuacao;
+    int maxAmmo;
     float prevShotTime = 0;
     bool shooting = false;
     
